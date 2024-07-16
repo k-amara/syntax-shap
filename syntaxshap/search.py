376a7739812007f118c447aad920b6552bdee8e5
kamara <22amara.kenza@gmail.com>
2024-07-16 14:01:07 +0200

    rank compute

--- syntaxshap/search.py
+++ syntaxshap/search.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 from utils._general import get_embedding_mask
 
-class SimilaritySearch():
+class RankSearch():
 
     def __init__(self, tokenizer, padding_length, seed, device):
         self.seed = seed
@@ -13,44 +13,31 @@ class SimilaritySearch():
         self.device = device
 
 
-    def __call__(self, embeddings, filtered_ids, embedding_data, args):
+    def __call__(self, embeddings, query_token_ids, sentence_id, args):
+        """
+        embeddings (n_token * size_voc * hidden_size): the embeddings of each token 
+                                                    in the vocabulary when replacing each token in the sentence.
+        query_token_ids: the token ids of the sentence
+        sentence_id
+        """
         
-        d = embeddings.shape[-1]
+        num_query_token, size_voc, hidden_size = embeddings.shape
+        # Initialize an array to store the extracted embeddings
+        query_embeddings = np.zeros((len(query_token_ids), hidden_size))
+
+        for i, token_id in enumerate(query_token_ids):
+            # Extract the embedding for the given token_id
+           query_embeddings[i] = embeddings[i, token_id, :]
+        query_embeddings = query_embeddings.detach().cpu().numpy()
         
-        mask, query_mask = get_embedding_mask(self.tokenizer, embedding_data, self.padding_length, filtered_ids)
-        xb = embeddings[query_mask]
-
-        if args.similarity_metric == "euclidean":
-            index = faiss.IndexFlatL2(d)
-            index.add(embeddings[mask])
-        elif args.similarity_metric == "cosine":
-            index = faiss.index_factory(d, "Flat", faiss.METRIC_INNER_PRODUCT)
-            embedding_vectors = embeddings[mask]
-            embedding_vectors = embedding_vectors.detach().cpu().numpy()
-            print(embedding_vectors.dtype, embedding_vectors.__class__, embedding_vectors.shape)
-            faiss.normalize_L2(embedding_vectors)
-            index.add(embedding_vectors)
-            xb = xb.detach().cpu().numpy()
-            faiss.normalize_L2(xb)
-
+        ranks_sentence_id = np.zeros((num_query_token, size_voc), dtype=np.int32)
+        for i, token_id in enumerate(query_token_ids):
+            embs = embeddings[i]
+            query_emb = query_embeddings[i]
+            faiss.normalize_L2(embs)
+            faiss.normalize_L2(query_emb)
+            index.add(embs)
+            ranks_sentence_id[i] = index.search(query_emb, size_voc)[1].astype(np.int32)
+        print(ranks_sentence_id.shape)
         
-        #num_tokens = hidden_indices_sample.sum(axis=1)
-        #tokens_cumulative = num_tokens.cumsum()
-
-        k = len(embeddings[mask])
-        print(k)
-        step_size = 500
-        #D = np.zeros((len(xb), k), dtype=np.float32)
-        num_query = len(xb)
-        I = np.zeros((num_query, k), dtype=np.int32)
-        start_time = time.time()
-        for i in range(0, num_query, step_size):
-            end_index = min(i + step_size, num_query)
-            I[i:end_index] = index.search(xb[i:end_index], k)[1].astype(np.int32)
-        print(time.time() - start_time)
-        #print(D)
-        #print(I)
-
-        print(I.shape)
-        
-        return None, I
+        return ranks_sentence_id
